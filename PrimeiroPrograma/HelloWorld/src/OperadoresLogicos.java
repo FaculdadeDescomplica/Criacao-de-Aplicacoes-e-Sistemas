6dc2672d9cbb6e94e8fbbcb88c69f0240f188fe7
Ana Teixeira <acrispteixeira@gmail.com>
2024-08-20 19:46:25 -0300

    Exemplo de aula: Operadores Lógicos

---
+++ PrimeiroPrograma/HelloWorld/src/OperadoresLogicos.java
@@ -0,0 +1,17 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        // Operador && (AND Lógico)
+        System.out.println("a && b: " + (a && b));  // false, porque b é false
+
+        // Operador || (OR Lógico)
+        System.out.println("a || b: " + (a || b));  // true, porque a é true
+
+        // Operador ! (NOT Lógico)
+        System.out.println("!a: " + (!a));  // false, porque a é true
+        System.out.println("!b: " + (!b));  // true, porque b é false
+    }
+}
