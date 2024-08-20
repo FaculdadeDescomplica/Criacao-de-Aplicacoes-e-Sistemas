67a70580faf4c159a1b3f9b783013d4486b5b4c9
Ana Teixeira <acrispteixeira@gmail.com>
2024-08-20 19:36:10 -0300

    Exemplo de aula: tabela verdade

---
+++ PrimeiroPrograma/HelloWorld/src/TabelaVerdade.java
@@ -0,0 +1,23 @@
+public class TabelaVerdade {
+    public static void main(String[] args) {
+        // Tabela Verdade para o Operador AND (&&)
+        System.out.println("Tabela Verdade para o Operador AND (&&):");
+        System.out.println("A\tB\tA && B");
+        System.out.println(true + "\t" + true + "\t" + (true && true)); // (true && true)
+        System.out.println(true + "\t" + false + "\t" + (false)); // (true && false)
+        System.out.println(false + "\t" + true + "\t" + (false)); // (false && true)
+        System.out.println(false + "\t" + false + "\t" + (false)); // (false && false)
+
+        // Linha em branco para separar as tabelas
+        System.out.println();
+
+        // Tabela Verdade para o Operador OR (||)
+
+        System.out.println("Tabela Verdade para o Operador OR (||):");
+        System.out.println("A\tB\tA || B");
+        System.out.println(true + "\t" + true + "\t" + (true || true)); // (true || true)
+        System.out.println(true + "\t" + false + "\t" + (true)); // (true || false)
+        System.out.println(false + "\t" + true + "\t" + (true)); // (false || true)
+        System.out.println(false + "\t" + false + "\t" + (false)); // (false || false)
+    }
+}
