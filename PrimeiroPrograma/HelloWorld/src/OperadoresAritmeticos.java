0e1b2a8096cc3851c73dbe543161a6525402760f
Ana Teixeira <acrispteixeira@gmail.com>
2024-08-20 19:17:44 -0300

    Exemplo de aula: Operadores Aritméticos

---
+++ PrimeiroPrograma/HelloWorld/src/OperadoresAritmeticos.java
@@ -0,0 +1,36 @@
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        int x = 15;
+        int y = 5;
+
+        int soma = x + y;       // Adição
+        System.out.println("Soma: " + soma);
+
+        int subtracao = x - y;  // Subtração
+        System.out.println("Subtração: " + subtracao);
+
+
+        int multiplicacao = x * y; // Multiplicação
+        System.out.println("Multiplicação: " + multiplicacao);
+
+        int divisao = x / y;    // Divisão
+        System.out.println("Divisão: " + divisao);
+
+        int modulo = x % y;     // Módulo (resto da divisão)
+        System.out.println("Módulo: " + modulo);
+
+        // Operadores de Incremento e Decremento
+        int incremento = 10;
+        int decremento = 10;
+
+        incremento++; // Incremento (incrementa em 1)
+        System.out.println("Valor após incremento: " + incremento);
+
+        decremento--; // Decremento (subtrai 1)
+        System.out.println("Valor após decremento: " + decremento);
+        
+    }
+
+}
