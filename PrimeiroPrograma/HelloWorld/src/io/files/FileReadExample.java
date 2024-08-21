ec6d2d0659cf90ae4c25ee12648b24c9734b579c
Ana Teixeira <acrispteixeira@gmail.com>
2024-08-20 21:33:00 -0300

    Exemplo de aula: Escrita e Leitura de Arquivo

---
+++ PrimeiroPrograma/HelloWorld/src/io/files/FileReadExample.java
@@ -0,0 +1,21 @@
+package io.files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+// LEITURA
+public class FileReadExample {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("arquivo.txt"))) {
+            String linha;
+            while (
+                    (linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ PrimeiroPrograma/HelloWorld/src/io/files/FileWriteExample.java
@@ -0,0 +1,21 @@
+package io.files;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// ESCRITA
+public class FileWriteExample {
+    public static void main(String[] args) {
+        try (
+                BufferedWriter writer = new BufferedWriter(
+                new FileWriter("arquivo.txt"))) {
+            writer.write("Olá, Mundo!");
+            writer.newLine();
+            writer.write("Segunda linha.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
